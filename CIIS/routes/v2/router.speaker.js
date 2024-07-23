444ea05be3676cdca4e5bbf51f074c4a98707239
kg-mc <kleingmamanic@gmail.com>
2024-07-23 10:09:31 -0500

    Update router.speaker.js

--- CIIS/routes/v2/router.speaker.js
+++ CIIS/routes/v2/router.speaker.js
@@ -3,6 +3,9 @@ const CONTROLLER_SPEAKER = require("../../controllers/v2/speaker.controller");
 const { authMid, isAdmin, isAtLeastOrganizer } = require("../../middlewares/v2/auth");
 const RouterSpeaker = Router();
 const speakerUpdateDTO = require("../../DTO/speaker.update.dto");
+const createSpeakerDTO = require("../../DTO/speaker.create.dto")
+const { validateFileOptional } = require("../../middlewares/validateExistenceOfRecord");
+const { createSpeaker } = require("../../controllers/speaker.controller");
 
 RouterSpeaker.route("/speakers").get(authMid, isAtLeastOrganizer,
   CONTROLLER_SPEAKER.GET
@@ -14,6 +17,7 @@ RouterSpeaker.route("/speakers/:id").get(authMid, isAtLeastOrganizer, CONTROLLER
 
 RouterSpeaker.route("/speakers/:id").delete(authMid, isAdmin, CONTROLLER_SPEAKER.DELETE);
 
+RouterSpeaker.post('/speakers', authMid, isAtLeastOrganizer, validateFileOptional("avatar", ["jpg", "jpeg", "png"]), createSpeakerDTO, createSpeaker);
 
 
 module.exports = RouterSpeaker;
