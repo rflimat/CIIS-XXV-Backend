464239c26898b9973fc1e34a835e1d01fd5f4b23
kg-mc <kleingmamanic@gmail.com>
2024-07-23 11:03:19 -0500

    Update router.topic.js

--- CIIS/routes/v2/router.topic.js
+++ CIIS/routes/v2/router.topic.js
@@ -2,9 +2,10 @@
 const { Router } = require("express");
 const RouterTopic = Router();
 const { getTopic, updateTopic, deleteTopic } = require("../../controllers/v2/topics.controller");
+const { getTopics } = require("../../controllers/topics.controller");
 
 RouterTopic.route("/topics/:id").get(getTopic)
 RouterTopic.route('/topics/:id').put(updateTopic)
 RouterTopic.route('/topics/:id').delete(deleteTopic)
-
+RouterTopic.get('/topics', getTopics);
 module.exports = RouterTopic;
